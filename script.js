bce30769e960acbc77bf48c38918852922376d5f
joaofauth <horlle.fauth@gmail.com>
2022-06-07 20:56:49 -0300

    NovaVersao

---
+++ script.js
@@ -0,0 +1,30 @@
+let titulo = document.querySelector('#titulo');
+let sinopse = document.querySelector('#sinopse');
+let url = document.querySelector('#url');
+let containerCard = document.querySelector("#containerCard");
+
+let botaoEnviar = document.querySelector('#botaoEnviar');
+
+botaoEnviar.addEventListener('click', function (event) {
+    event.preventDefault();
+    callback();
+
+})
+function callback() {
+    let card = `<div class="card m-2" style="width: 18rem;">
+                    <img src="${url.value}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${titulo.value}</h5>
+                        <p class="card-text">${sinopse.value}</p>
+                    </div>
+                </div>`
+               
+    let novoCard = document.createElement('div');
+    novoCard.classList.add("novoCard")
+    novoCard.innerHTML = card;
+    containerCard.appendChild(novoCard);
+}
+
+
+
+
