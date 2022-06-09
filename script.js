beb02275a6ba799dc025ec86ff6c8fca83da6a1b
joaofauth <horlle.fauth@gmail.com>
2022-06-09 19:43:55 -0300

    Alterações visuais

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let titulo = document.querySelector('#titulo');
 let sinopse = document.querySelector('#sinopse');
 let url = document.querySelector('#url');
 let containerCard = document.querySelector("#containerCard");
+let nota = document.querySelector("#nota");
 
 let botaoEnviar = document.querySelector('#botaoEnviar');
 
@@ -12,11 +13,12 @@ botaoEnviar.addEventListener('click', function (event) {
 })
 function callback() {
     let card = `
-                <div class="card m-2" style="width: 22rem;">
+                <div class="card me-3 mt-5  card-style" style="width: 20rem;">
                     <img src="${url.value}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">${titulo.value}</h5>
+                        <h5 class="card-title card-titulo"><b>${titulo.value}</b></h5>
                         <p class="card-text">${sinopse.value}</p>
+                        <h class="card-text card-nota"><b>${nota.value}</b></h6>
                     </div>
                 </div>
                 `
